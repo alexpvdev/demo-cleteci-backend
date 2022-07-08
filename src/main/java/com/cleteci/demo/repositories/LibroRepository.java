ac36fe6eb32c71d01fc9d7f496194574f8087ceb
Luis Peña <alexpv3125@gmail.com>
2022-07-07 22:09:55 -0400

    Se agrega configuracion de bbdd h2

--- src/main/java/com/cleteci/demo/repositories/LibroRepository.java
+++ src/main/java/com/cleteci/demo/repositories/LibroRepository.java
@@ -2,9 +2,11 @@ package com.cleteci.demo.repositories;
 
 import com.cleteci.demo.entities.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
     List<Libro> findLibroByTitulo(String titulo);
     long deleteLibroByTitulo(String titulo);
